f81ab8b79b86e32cf19a29d79c409f83a3c050ff
baongoc02 <20110276@student.hcmute.edu.vn>
2023-04-24 19:08:32 +0700

    Update assertions.py

--- assertions.py
+++ assertions.py
@@ -3,14 +3,11 @@ import time
 def assert_with_time_out(func, time_out):
     start = time.time()
     result = None
-    while True:
+    while not result:
         try:
             result = func()
-            break
         except:
             end = time.time()
             if end - start > time_out:
                 raise Exception("Wrong test case")
             time.sleep(0.1)
-    if not result:
-        raise Exception("Wrong test case")
